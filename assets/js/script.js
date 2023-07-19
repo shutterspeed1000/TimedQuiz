be9cfab8f535b6bb9f92671955de47f5d01b2a21
Sean Worrell <spworrell@gmail.com>
2023-07-19 19:48:34 -0400

    Timer Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
 
+//Timer for game/updates header
+
+var timeEl = document.getElementById("time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 10;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+    console.log(secondsLeft);
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// // Function to create and append colorsplosion image
+// function sendMessage() {
+//   timeEl.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+
+
+setTime();
