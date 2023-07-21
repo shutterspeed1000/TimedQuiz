487caf680578a3807f85a265ae817c1cc1737957
Sean Worrell <spworrell@gmail.com>
2023-07-21 15:00:18 -0400

    InnerHTML Working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 
 //Timer for game/updates header
-
 var timeEl = document.getElementById("time");
 
-// Selects element by id
+// Question Bank for Quiz
+
+var qbank = {
+  question: "What is the HTML attribute used to define the internal CSS stylesheet?",
+  a1: "styles",
+  a2: "class",
+  a3: "style",
+  a4: "variable",
+  ca: "style",
+  qnum: "Question 1"
+};
 
 
 var secondsLeft = 10;
@@ -25,40 +34,27 @@ function setTime() {
   }, 1000);
 }
 
-document.getElementById("mainsection").innerHTML = '${secondsLeft}'
+document.getElementById("mainsection").innerHTML= 
+
+`<h1>Time ${qbank.question}</h1>
+  <div class = qbox>
+  
+  <button>${qbank.a1}</button>
+  <button>${qbank.a2}</button>
+  <button>${qbank.a3}</button>
+  <button>${qbank.a4}</button>
+
+  </div>
 
 
-
-
-
-
-
-
-
-
+`;
 
 setTime();
 
 
 
-function question(){
-
-}
 
 
 
-
-
-// Question Bank Vars as Objects
-
-var q1 = {
-    question: "What is the HTML attribute used to define the internal CSS stylesheet?",
-    a1: "styles",
-    a2: "class",
-    a3: "style",
-    a4: "variable",
-    ca: "style",
-    qnum: "1"
-}
 
 
