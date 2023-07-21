856b6e6a580e2c1426747ea5d5e6c257c1c18c6c
Sean Worrell <spworrell@gmail.com>
2023-07-21 15:44:36 -0400

    Start BTN Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,93 @@
-
 //Timer for game/updates header
 var timeEl = document.getElementById("time");
+var startBtn = document.getElementById("start")
 
 // Question Bank for Quiz
 
-var qbank = {
-  question: "What is the HTML attribute used to define the internal CSS stylesheet?",
-  a1: "styles",
+var qbank1 = {
+  question:
+    "What is the HTML attribute used to define the internal CSS stylesheet?",
+  a1: "Styles",
   a2: "class",
   a3: "style",
   a4: "variable",
   ca: "style",
-  qnum: "Question 1"
+  qnum: "Question 1",
 };
 
+var qbank2 = {
+  question:
+    "Questions 2",
+  a1: "Farts",
+  a2: "class",
+  a3: "style",
+  a4: "variable",
+  ca: "style",
+  qnum: "Question 1",
+};
+
+var qbank3 = {
+  question:
+    "Question 3",
+  a1: "Styles",
+  a2: "class",
+  a3: "style",
+  a4: "variable",
+  ca: "style",
+  qnum: "Question 1",
+};
+
+var qbank4 = {
+  question:
+    "Question 4?",
+  a1: "Styles",
+  a2: "class",
+  a3: "style",
+  a4: "variable",
+  ca: "style",
+  qnum: "Question 1",
+};
 
 var secondsLeft = 10;
 
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function() {
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
     console.log(secondsLeft);
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-    //   sendMessage();
+      //   sendMessage();
     }
-
   }, 1000);
 }
 
-document.getElementById("mainsection").innerHTML= 
 
-`<h1>Time ${qbank.question}</h1>
+function askQuestion() {
+//start time when quiz starts
+
+setTime()
+
+
+var qnum = qbank2
+
+  document.getElementById("mainsection").innerHTML = `<h1>Time ${qnum.question}</h1>
   <div class = qbox>
-  
-  <button>${qbank.a1}</button>
-  <button>${qbank.a2}</button>
-  <button>${qbank.a3}</button>
-  <button>${qbank.a4}</button>
+
+  <button class= "answer">${qnum.a1}</button>
+  <button class= "answer">${qnum.a2}</button>
+  <button class= "answer">${qnum.a3}</button>
+  <button class= "answer">${qnum.a4}</button>
 
   </div>
 
 
 `;
-
-setTime();
-
+}
 
 
-
-
-
-
-
+// askQuestion();
