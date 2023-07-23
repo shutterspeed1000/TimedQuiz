007680018bc1d217c75a34916dfad5e6c71494c1
Sean Worrell <spworrell@gmail.com>
2023-07-22 21:54:12 -0400

    helpme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,65 @@
-//Timer for game/updates header
-var timeEl = document.getElementById("time");
-var startBtn = document.getElementById("start")
 
 // Question Bank for Quiz
 
-var qbank1 = {
-  question:
-    "What is the HTML attribute used to define the internal CSS stylesheet?",
-  a1: "Styles",
-  a2: "class",
-  a3: "style",
-  a4: "variable",
-  ca: "style",
-  qnum: "Question 1",
-};
+const qbank = [
+  {
+    question:
+      "What is the HTML attribute used to define the internal CSS stylesheet?",
+    a1: "Styles",
+    a2: "class",
+    a3: "style",
+    a4: "variable",
+    ca: "style",
+    qnum: "Question 1",
+  },
 
-var qbank2 = {
-  question:
-    "Questions 2",
-  a1: "Test",
-  a2: "class",
-  a3: "style",
-  a4: "variable",
-  ca: "style",
-  qnum: "Question 1",
-};
+  {
+    question: "Questions 2",
+    a1: "Test",
+    a2: "class",
+    a3: "style",
+    a4: "variable",
+    ca: "style",
+    qnum: "Question 1",
+  },
 
-var qbank3 = {
-  question:
-    "Question 3",
-  a1: "Styles",
-  a2: "class",
-  a3: "style",
-  a4: "variable",
-  ca: "style",
-  qnum: "Question 1",
-};
+  {
+    question: "Question 3",
+    a1: "Styles",
+    a2: "class",
+    a3: "style",
+    a4: "variable",
+    ca: "style",
+    qnum: "Question 1",
+  },
 
-var qbank4 = {
-  question:
-    "Question 4?",
-  a1: "Styles",
-  a2: "class",
-  a3: "style",
-  a4: "variable",
-  ca: "style",
-  qnum: "Question 1",
-};
+  {
+    question: "Question 4?",
+    a1: "a4",
+    a2: "class",
+    a3: "style",
+    a4: "variable",
+    ca: "style",
+    qnum: "Question 1",
+  },
+];
+
+
+//Timer for game/updates header
+var timeEl = document.getElementById("time");
+var startBtn = document.getElementById("start");
+var questionPicked = Math.floor(Math.random() * 3);
+ startBtn.addEventListener("click", function () {
+askQuestion()
+})
+
+
+var questionPicked = Math.floor(Math.random() * 3);
+
+
+
+
+
 
 var secondsLeft = 10;
 
@@ -66,27 +79,33 @@ function setTime() {
   }, 1000);
 }
 
-
 function askQuestion() {
-//start time when quiz starts
-setTime()
-//Loop Here....
 
-var qnum = qbank2
+  document.getElementById("mainsection").innerHTML = `<h1>${qbank[questionPicked].question}</h1>
 
-  document.getElementById("mainsection").innerHTML = `<h1>${qnum.question}</h1>
   <div class = qbox>
 
-  <button class= "answer">${qnum.a1}</button>
-  <button class= "answer">${qnum.a2}</button>
-  <button class= "answer">${qnum.a3}</button>
-  <button class= "answer">${qnum.a4}</button>
+  <button class= "answer" id="ba1">${qbank[questionPicked].a1}</button>
+  <button class= "answer" id="ba2">${qbank[questionPicked].a2}</button>
+  <button class= "answer" id="ba3">${qbank[questionPicked].a3}</button>
+  <button class= "answer" id="ba4">${qbank[questionPicked].a4}</button>
 
-  </div>
+  </div>`
+
+  var answerClicked
+
+  // Buttons for answers
+  var ba1 = document.getElementById("ba1");
+  var ba2 = document.getElementById("ba2");
+  var ba3 = document.getElementById("ba3");
+  var ba4 = document.getElementById("ba4");
+  
+  ba1.addEventListener("click", function () {
+    var answerClicked = 1
+   })
+  
+   console.log(answerClicked) 
 
 
-`;
 }
 
-
-// askQuestion();
