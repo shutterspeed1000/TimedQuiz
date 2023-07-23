d88271d2ada9cbe14af6f7503f06fee1774f5fcf
Sean Worrell <spworrell@gmail.com>
2023-07-23 00:45:32 -0400

    Reworked HTML, Removed loop, using attributes instead of innerhtml

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // Question Bank for Quiz
 
 const qbank = [
@@ -9,8 +8,7 @@ const qbank = [
     a2: "class",
     a3: "style",
     a4: "variable",
-    ca: "style",
-    qnum: "Question 1",
+    ca: "1",
   },
 
   {
@@ -19,8 +17,7 @@ const qbank = [
     a2: "class",
     a3: "style",
     a4: "variable",
-    ca: "style",
-    qnum: "Question 1",
+    ca: "3",
   },
 
   {
@@ -29,8 +26,8 @@ const qbank = [
     a2: "class",
     a3: "style",
     a4: "variable",
-    ca: "style",
-    qnum: "Question 1",
+    ca: "2",
+   
   },
 
   {
@@ -39,29 +36,31 @@ const qbank = [
     a2: "class",
     a3: "style",
     a4: "variable",
-    ca: "style",
-    qnum: "Question 1",
+    ca: "1",
+    
   },
 ];
 
-
 //Timer for game/updates header
 var timeEl = document.getElementById("time");
+
 var startBtn = document.getElementById("start");
-var questionPicked = Math.floor(Math.random() * 3);
- startBtn.addEventListener("click", function () {
-askQuestion()
-})
 
+var titlepage = document.getElementById("titlepage");
+var questionpage = document.getElementById("questionpage");
+var endpage = document.getElementById("endpage");
+var question = document.getElementById("question");
+var ba1 = document.getElementById("ba1");
+var ba2 = document.getElementById("ba2");
+var ba3 = document.getElementById("ba3");
+var ba4 = document.getElementById("ba4");
 
-var questionPicked = Math.floor(Math.random() * 3);
+startBtn.addEventListener("click", function () {
+  setTime();
+  askQuestion();
+});
 
-
-
-
-
-
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 function setTime() {
   // Sets interval in variable
@@ -80,32 +79,93 @@ function setTime() {
 }
 
 function askQuestion() {
+  titlepage.setAttribute("style", "display: none");
+  questionpage.setAttribute("style", "display: block");
 
-  document.getElementById("mainsection").innerHTML = `<h1>${qbank[questionPicked].question}</h1>
+  var questionPicked = Math.floor(Math.random() * 3);
 
-  <div class = qbox>
+  // Set text for q and as
+  question.textContent = `${qbank[questionPicked].question}`;
+  ba1.textContent = `${qbank[questionPicked].a1}`;
+  ba2.textContent = `${qbank[questionPicked].a2}`;
+  ba3.textContent = `${qbank[questionPicked].a3}`;
+  ba4.textContent = `${qbank[questionPicked].a4}`;
+  currentAnswer = qbank[questionPicked].ca;
 
-  <button class= "answer" id="ba1">${qbank[questionPicked].a1}</button>
-  <button class= "answer" id="ba2">${qbank[questionPicked].a2}</button>
-  <button class= "answer" id="ba3">${qbank[questionPicked].a3}</button>
-  <button class= "answer" id="ba4">${qbank[questionPicked].a4}</button>
-
-  </div>`
-
-  var answerClicked
-
-  // Buttons for answers
-  var ba1 = document.getElementById("ba1");
-  var ba2 = document.getElementById("ba2");
-  var ba3 = document.getElementById("ba3");
-  var ba4 = document.getElementById("ba4");
-  
-  ba1.addEventListener("click", function () {
-    var answerClicked = 1
-   })
-  
-   console.log(answerClicked) 
-
-
+  return currentAnswer
 }
 
+// Check for correct answer when button pushed
+
+
+var answerClicked
+
+
+ba1.addEventListener("click", function () {
+  answerClicked = 1;
+  alert(1);
+  if (answerClicked == currentAnswer) {
+    alert("Correct");
+    askQuestion();
+  } else {
+    secondsLeft = secondsLeft - 5;
+  }
+});
+
+ba2.addEventListener("click", function () {
+  answerClicked = 2;
+  alert(2);
+  if (answerClicked == currentAnswer) {
+    alert("Correct");
+    askQuestion();
+  } else {
+    secondsLeft = secondsLeft - 5;
+  }
+});
+
+ba3.addEventListener("click", function () {
+  alert(3);
+  answerClicked = 3;
+  if (answerClicked == currentAnswer) {
+    alert("Correct");
+    askQuestion();
+  } else {
+    secondsLeft = secondsLeft - 5;
+  }
+});
+
+ba4.addEventListener("click", function () {
+  alert(4);
+  answerClicked = 4;
+  if (answerClicked == currentAnswer) {
+    alert("Correct");
+    askQuestion();
+  } else {
+    secondsLeft = secondsLeft - 5;
+  }
+});
+
+// document.getElementById("mainsection").innerHTML = `<h1>${qbank[questionPicked].question}</h1>`
+
+//   <div class = qbox>
+
+//   <button class= "answer" id="ba1">${qbank[questionPicked].a1}</button>
+//   <button class= "answer" id="ba2">${qbank[questionPicked].a2}</button>
+//   <button class= "answer" id="ba3">${qbank[questionPicked].a3}</button>
+//   <button class= "answer" id="ba4">${qbank[questionPicked].a4}</button>
+
+//   </div>`
+//   var corectAnswer = qbank[qbank[questionPicked].ca]
+//   var answerClicked
+
+//   // Buttons for answers
+//   var ba1 = document.getElementById("ba1");
+//   var ba2 = document.getElementById("ba2");
+//   var ba3 = document.getElementById("ba3");
+//   var ba4 = document.getElementById("ba4");
+
+// ba1.addEventListener("click", function () {
+//   var answerClicked = 1
+//   })
+
+// return corectAnswer,answerClicked
